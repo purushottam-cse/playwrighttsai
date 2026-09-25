33946f98505648ac4d2e2b9d0469023456f8e40a
purushottam-cse <purushottam-cse@users.noreply.github.com>
2026-09-25 16:52:37 +0530

    adding let var const file

--- 6_Function/Var_Let_Const.js
+++ 6_Function/Var_Let_Const.js
@@ -4,3 +4,31 @@ console.log(a)
 
 
 console.log(b)
+var b = 'puru'
+console.log(b)
+
+
+// console.log(c)
+// let c= 'puru'
+// console.log(c)
+
+
+// TDZ temporal died zone 
+
+var apple = 'fruit'
+
+if(true){
+    console.log(apple)   // value of apple is undefined is in TDZ when var is used 
+    var apple = 'exoctic fruit'
+}
+
+let fruit = 'Banana'
+
+if (true){
+    console.log(fruit)   // value of out scope, variable with no value because declared value is out of scope
+    let fruit = 'Kela'
+}
+
+const score = 100;
+// score = score + 10 cannot assign to constant valriable 
+console.log(score)
